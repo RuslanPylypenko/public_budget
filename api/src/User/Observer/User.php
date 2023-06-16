6c4bdb33e20ceba6666bf38c3d426099e69276ee
Ruslan Pylypenko <r.pylypenko@m2e.team>
2023-06-16 23:31:21 +0300

    send mail

--- api/src/User/Observer/User.php
+++ api/src/User/Observer/User.php
@@ -4,9 +4,18 @@ namespace App\User\Observer;
 
 use App\User\Events\UserRegisteredEvent;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
 
 class User implements EventSubscriberInterface
 {
+    public function __construct(
+        private MailerInterface $mailer,
+        private string $appUrl,
+        private string $adminEmail,
+    ) {
+    }
+
     public static function getSubscribedEvents()
     {
         return [
@@ -18,6 +27,14 @@ class User implements EventSubscriberInterface
 
     public function onUserRegister(UserRegisteredEvent $event): void
     {
+        $endpoint = sprintf($this->appUrl . '/user/confirm/%s', $event->getUser()->getConfirmToken()->getToken());
 
+        $email = (new Email())
+            ->from($this->adminEmail)
+            ->to($event->getUser()->getEmail())
+            ->subject('Welcome!!!')
+            ->html(sprintf('Welcome, you are registered! go to <a href="%s">Here to confirmation</a>', $endpoint));
+
+        $this->mailer->send($email);
     }
 }
