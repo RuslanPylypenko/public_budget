44ac2eea679c70fd0f050bd0863edd9a0de59b17
Serhii Herych <mac@MacBook-Air-mac.local>
2023-07-23 14:30:10 +0300

    Test commit front

--- frontend2/src/App.js
+++ frontend2/src/App.js
@@ -9,6 +9,11 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quis eligendi eum consequatur recusandae debitis odit voluptatibus ad, vitae in, quibusdam iusto animi officiis tempore dolorum, corrupti dolorem perferendis impedit cupiditate!
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Est dolores corrupti dignissimos consequuntur cumque molestias repellendus nisi quas necessitatibus beatae saepe officia aliquam iure velit aut porro, ea dolor cum.
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magni nesciunt voluptatum odit sequi perspiciatis in. In excepturi quas animi cum. Qui, culpa cumque incidunt nemo ex alias deserunt dicta quidem.
+
         <a
           className="App-link"
           href="https://reactjs.org"
