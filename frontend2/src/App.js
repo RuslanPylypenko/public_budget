77a80eb486e0e88c1cf38a45ed1ab4ef6e3420e8
Serhii Herych <mac@MacBook-Air-mac.local>
2023-07-23 15:27:57 +0300

    first requst

--- frontend2/src/App.js
+++ frontend2/src/App.js
@@ -1,30 +1,31 @@
-import logo from './logo.svg';
+import React, { useEffect } from 'react';
+
 import './App.css';
 
 function App() {
+  useEffect(() => {
+    fetch('http://localhost:8081/api/projects/1/', {
+      method: 'POST',
+      body: JSON.stringify({
+        result: {
+          limit: 10,
+          offset: 0
+        }
+      })
+    }).then((res) => res.json()).then(json => console.log(json)).catch(err => console.warn(err))
+  }, [])
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quis eligendi eum consequatur recusandae debitis odit voluptatibus ad, vitae in, quibusdam iusto animi officiis tempore dolorum, corrupti dolorem perferendis impedit cupiditate!
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Est dolores corrupti dignissimos consequuntur cumque molestias repellendus nisi quas necessitatibus beatae saepe officia aliquam iure velit aut porro, ea dolor cum.
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magni nesciunt voluptatum odit sequi perspiciatis in. In excepturi quas animi cum. Qui, culpa cumque incidunt nemo ex alias deserunt dicta quidem.
-
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
 }
 
 export default App;
+
+
+// http://localhost:8081/api/projects/find/
+// http://localhost:8081/api/get/
