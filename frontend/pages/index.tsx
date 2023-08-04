599f757ebb3239699ed6ec3086a81cb2cc2946dc
NataliaPylypenko <natalia28ivanivna@gmail.com>
2023-08-04 09:51:21 +0300

    Heading component

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,10 +1,12 @@
+import React from "react";
+import {Htag} from "../components";
 
 export default function Home(): JSX.Element {
   return (
       <>
-        <main>
-          <h1>Hello React + Next.js</h1>
-        </main>
+          <Htag tag='h1'>Heading level 1</Htag>
+          <Htag tag='h2'>Heading level 2</Htag>
+          <Htag tag='h3'>Heading level 3</Htag>
       </>
   );
 }
