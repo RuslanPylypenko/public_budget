48e3982af3a0b8f616397aaaece1a57ff887927e
NataliaPylypenko <natalia28ivanivna@gmail.com>
2023-08-07 13:14:35 +0300

    updated

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -2,11 +2,17 @@ module.exports = {
     webpack(config, options) {
         config.module.rules.push({
             loader: '@svgr/webpack',
+            issuer: /\.[jt]sx?$/,
             options: {
                 prettier: false,
                 svgo: true,
                 svgoConfig: {
-                    plugins: [{ removeViewBox: false }]
+                    plugins: [{
+                        name: 'preset-default',
+                        params: {
+                            removeViewBox: false
+                        }
+                    }]
                 },
                 titleProp: true,
             },
