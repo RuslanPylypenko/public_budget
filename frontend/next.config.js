bf9fc990096c185dbd5c1d8e943beff97a06b1aa
NataliaPylypenko <natalia28ivanivna@gmail.com>
2023-08-07 09:20:46 +0300

    button component

---
+++ frontend/next.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+    webpack(config, options) {
+        config.module.rules.push({
+            loader: '@svgr/webpack',
+            options: {
+                prettier: false,
+                svgo: true,
+                svgoConfig: {
+                    plugins: [{ removeViewBox: false }]
+                },
+                titleProp: true,
+            },
+            test: /\.svg$/,
+        });
+
+        return config;
+    },
+};
--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import {Htag} from "../components";
+import {Button, Htag} from "../components";
 
 export default function Home(): JSX.Element {
   return (
@@ -7,6 +7,10 @@ export default function Home(): JSX.Element {
           <Htag tag='h1'>Heading level 1</Htag>
           <Htag tag='h2'>Heading level 2</Htag>
           <Htag tag='h3'>Heading level 3</Htag>
+
+          <Button appearance='primary'>Button Primary</Button>
+          <Button appearance='accent' arrow>Button Accent</Button>
+          <Button appearance='ghost'>Button Ghost</Button>
       </>
   );
 }
