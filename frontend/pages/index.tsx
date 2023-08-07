08798b346fd107fc454ce77991099a9ffa5e3e54
NataliaPylypenko <natalia28ivanivna@gmail.com>
2023-08-07 15:52:55 +0300

    paragraph component

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import {Button, Htag} from "../components";
+import {Button, Htag, P} from "../components";
 
 export default function Home(): JSX.Element {
   return (
@@ -11,6 +11,12 @@ export default function Home(): JSX.Element {
           <Button appearance='primary'>Button Primary</Button>
           <Button appearance='accent' arrow>Button Accent</Button>
           <Button appearance='ghost'>Button Ghost</Button>
+
+          <P size='p1'>Paragraph size 1</P>
+          <P size='p2'>Paragraph size 2</P>
+          <P size='p3'>Paragraph size 3</P>
+          <P size='p4'>Paragraph size 4</P>
+          <P size='p5'>Paragraph size 5</P>
       </>
   );
 }
