36a5bd144562cfa21cf8cb22a1877c12bc20cbf5
NataliaPylypenko <natalia28ivanivna@gmail.com>
2023-08-24 12:57:54 +0300

    using getStaticProps

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,6 +1,6 @@
+import { AppProps } from "next/dist/shared/lib/router/router";
 import Head from "next/head";
 import React from "react";
-import { AppProps } from 'next/dist/next-server/lib/router/router';
 import '../styles/global.scss';
 
 export default function MyApp({ Component, pageProps }: AppProps): JSX.Element {
