c02abac762a9fcb89c044986a1df5ce0e0b6f2b8
Ruslan Pylypenko <r.pylypenko@m2e.team>
2023-10-17 22:04:12 +0300

    fix load statistic

--- frontend-react/src/pages/Home/Home.jsx
+++ frontend-react/src/pages/Home/Home.jsx
@@ -13,10 +13,9 @@ export function Home() {
             .then( response => response.json())
             .then( data => {
                 setCity(data.city);
-                return data.city
+                return city.currentSession.stages.find(stage => new Date > new Date(stage.startDate) && new Date() < new Date(stage.endDate));
             })
-            .then( city => {
-                let stage = city.currentSession.stages.find(stage => new Date > new Date(stage.startDate) && new Date() < new Date(stage.endDate));
+            .then( stage => {
                 fetch(`http://lviv.pb.local:8081/api/sessions/statistic/${stage.name}/`, {
                     method: 'GET',
                 })
