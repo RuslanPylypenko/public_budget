f978c948eea8070f66674bbacd6d26eacc6bc932
Ruslan <ruslan.php.dev@gmail.com>
2024-02-04 07:58:52 +0200

    remove frontend

--- frontend-react/src/components/index.jsx
+++
@@ -1,5 +0,0 @@
-export * from './Container/Container';
-export * from './Htag/Htag';
-export * from './Button/Button';
-export * from './Ptag/Ptag';
-export * from './Label/Label';
--- frontend-react/src/sections/index.jsx
+++
@@ -1,7 +0,0 @@
-export * from './Header/Header';
-export * from './Footer/Footer';
-export * from './Promo/Promo';
-export * from './Map/Map';
-export * from './Timeline/Timeline';
-export * from './Projects/Projects';
-export * from './Statistics/Statistics';
