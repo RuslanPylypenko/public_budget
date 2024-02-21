78a3a4ed9159ac9d40abb28bdbd282cd7ce733df
Ruslan <ruslan.php.dev@gmail.com>
2024-02-21 10:53:31 +0200

    search like

--- api/src/Project/Query/Find/Filter/Search.php
+++ api/src/Project/Query/Find/Filter/Search.php
@@ -16,13 +16,15 @@ readonly class Search implements DecoratorInterface
 
     public function apply(QueryBuilder $queryBuilder): void
     {
-       $conditions = [
-           $queryBuilder->expr()->like('p.name', ":search"),
-           $queryBuilder->expr()->like('p.short', ":search"),
-           $queryBuilder->expr()->like('p.description', ":search"),
-       ];
+        if($this->search){
+            $conditions = [
+                $queryBuilder->expr()->like('p.name', ":search"),
+                $queryBuilder->expr()->like('p.short', ":search"),
+                $queryBuilder->expr()->like('p.description', ":search"),
+            ];
 
-       $queryBuilder->andWhere($queryBuilder->expr()->orX(...$conditions));
-       $queryBuilder->setParameter('search', "%{$this->search}%");
+            $queryBuilder->andWhere($queryBuilder->expr()->orX(...$conditions));
+            $queryBuilder->setParameter('search', "%{$this->search}%");
+        }
     }
 }
